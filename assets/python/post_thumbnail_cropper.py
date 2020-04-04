49976bc007e7e4a9ba4e3a36c35ddf597072aebd
Andrei Tenescu <teny96@gmail.com>
2020-04-04 10:32:52 +0300

    Blog layout changes

---
+++ assets/python/post_thumbnail_cropper.py
@@ -0,0 +1,5 @@
+import glob
+
+input_folder = '/home/tzeny/Other/Projects/Websites/static_blog/tzeny.github.io/_posts/blog'
+assets_folder = '/home/tzeny/Other/Projects/Websites/static_blog/tzeny.github.io/assets/img/posts'
+
